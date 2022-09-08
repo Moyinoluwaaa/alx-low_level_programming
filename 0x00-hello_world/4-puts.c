6d44974bf3bc830e9e8a605a195239973f839125
Moyinoluwaaa <omopariolafaith@gmail.com>
2022-09-08 16:31:06 -0700

    this puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main is the entry point
+ * return: goes back to the main function
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return 0;
+}
+
