16ee9891717182b855391aa852cafe6ec17485b9
Moyinoluwaaa <omopariolafaith@gmail.com>
2022-09-09 13:49:33 -0700

    high

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: ends the program
+*/
+int main(void)
+{
+	int w = 0;
+	int e;
+
+	for(; w < 10; w++)
+	{
+	putchar(w + '0');
+	}
+	putchar('\n');
+	return (0);
+}
