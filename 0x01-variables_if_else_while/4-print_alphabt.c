e6b7d62dcfe50a06e4b2dfcbe0ba9d05f4bcf4f5
Moyinoluwaaa <omopariolafaith@gmail.com>
2022-09-09 12:16:16 -0700

    qe

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: ends the program
+*/
+int main(void)
+{
+	int alph = 'a';
+
+	for (int alph='a'; alph <= 'z'; alph++)
+	{
+		if (alph == 'q' || alph == 'e')
+	{
+		continue;
+	}
+	putchar(alph);
+	}   
+	putchar('\n');
+	return (0);
+}
+
