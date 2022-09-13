eed71d222f1680838832091442e62e46b3ea039c
Moyinoluwaaa <omopariolafaith@gmail.com>
2022-09-13 14:42:54 -0700

    checks for lowercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,15 +2,16 @@
 /**
 * _islower - checks for lowercase
 *
+* c - is tested for
 * Return: ends the program
 */
 
 int _islower(int c)
 {
-	if (c>='a' && c<='z')
+	if (c >= 'a' && c <= 'z')
 		return (1);
 	else
 		return (0);
 putchar('\n');
-return 0;
+return(0);
 }
