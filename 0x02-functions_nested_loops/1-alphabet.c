df6b6743c98a999ed935f88b25200203f7dcc35c
Moyinoluwaaa <omopariolafaith@gmail.com>
2022-09-13 13:21:16 -0700

    low

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,17 +2,19 @@
 /**
 * main - Entry point
 *
-* print_alphabet - prints alphabet
+* print_alphabet - prints lowetrcase alphabet
 *
 * Return: ends the program
 */
 
-void print_alphabet(void){
-    int w = 'a';
-    for (; w <= 'z'; w++)
-{
-    putchar(w);
-}
+void print_alphabet(void)
+	{
+	int w = 'a';
+	for (; w <= 'z'; w++)
+	{
+		putchar(w);
+	}
+putchar('\n');
 return;
 }
 
