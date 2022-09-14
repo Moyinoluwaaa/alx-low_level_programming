603cf561b1cb29617973aea7a227fb4c019f4ad4
Moyinoluwaaa <omopariolafaith@gmail.com>
2022-09-14 05:32:13 -0700

    test num

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,15 +11,16 @@ int print_sign(int n)
 if (n >= 0)
 {
 	putchar('+');
-	return (1);
+	putchar('1');
 }
 else if(n <= 0)
 {
-        return (-1);
+	putchar('-');
+        putchar('1');
 }
 else if (n == 0)
 {
-	return (0);
+	putchar('0');
 	putchar('0'); 
 }
 return (0);
