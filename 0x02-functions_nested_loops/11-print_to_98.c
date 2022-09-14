80b40083ef346182ef0b6a7c77cbe8fd723ae5a4
Moyinoluwaaa <omopariolafaith@gmail.com>
2022-09-14 08:49:02 -0700

    natural num

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_to_98 - func name
+ * @n: param
+ *
+ * Return: returns any
+ */
+void print_to_98(int n)
+
+{
+	for (n = 0; n < 98; n++)
+	{
+		_putchar(n + '0');
+		_putchar(',');
+		_putchar('');
+	}
+}
