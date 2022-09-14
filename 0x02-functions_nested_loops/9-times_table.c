e05bb144652ba28cec5ecb14b5b1e079c65b7930
Moyinoluwaaa <omopariolafaith@gmail.com>
2022-09-14 09:40:36 -0700

    multip[ly

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ * times_table - prints times table
+ *
+ * Return:void
+ *
+ */
+void times_table(void)
+{
+int a = 0;
+int b, multi;
+
+while (a <= 9)
+{
+	b = 0;
+	while (b <= 9)
+	{
+		rep = a * b;
+		if (b == 0)
+		{
+
+			_putchar(multi + '0');
+		}
+		else if (rep < 10)
+		{
+			_putchar(' ');
+			_putchar(multi + '0');	
+		}
+		else
+		{							        			_putchar(multi / 10 + '0');
+			_putchar(multi % 10 + '0');
+		}
+		if (b < 9)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+		b++;
+	}
+	_putchar('\n');
+	a++;
+}
+}
