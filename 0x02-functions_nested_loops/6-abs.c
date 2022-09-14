7fdb96edad9adf2a25e2cc9f3667a934f44f2a2d
Moyinoluwaaa <omopariolafaith@gmail.com>
2022-09-14 06:47:12 -0700

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs(int) - func name
+ * @n: the dtype
+ *
+ * return - ends the program
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	return (0);
+}
