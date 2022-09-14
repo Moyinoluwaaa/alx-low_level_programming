516eb5867522637a8f6dcf4d20e5f680932b4935
Moyinoluwaaa <omopariolafaith@gmail.com>
2022-09-14 05:09:22 -0700

    test num

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* print_sign - the func name
+* @n: the parameter
+*
+* Return: ends the program
+*/
+int print_sign(int n)
+{
+if (n >= 0)
+{
+        return (1);
+        putchar('+');
+}
+else if(n <= 0)
+{
+        return (-1);
+        putchar('-');
+}
+else if (n == 0)
+{
+	return (0);
+	putchar('0'); 
+}
+return (0);
+}
+
