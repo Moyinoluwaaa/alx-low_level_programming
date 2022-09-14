81150d8d7880800984b5f13420493e9d743f1c9d
Moyinoluwaaa <omopariolafaith@gmail.com>
2022-09-14 07:26:31 -0700

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,6 +8,18 @@
 int print_last_digit(int n)
 {
 	n = n %  10;
+	if (n > 0)
+	{
+		n = n % 10;
+		_putchar(n + '0');
+		return (n);
+	}
+	else if (n == 0)
+	{
+		n = n % 10;
+		_putchar(n + '0');
+		return (n);
+	}
 	if (n < 0)
 	{
 		n = -n;
