e8c31317c3cd9e6a059607ea3b0beb67419b4f1c
Moyinoluwaaa <omopariolafaith@gmail.com>
2022-09-14 12:36:05 -0700

    natural num

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,12 +6,32 @@
  * Return: returns any
  */
 void print_to_98(int n)
-
 {
-	for (i = 0; i < 98; n++)
+	int i = 0;
+
+	if (n > 98)
 	{
-		_putchar(n + '0');
-		_putchar(',');
-		_putchar(' ');
+		for (i = n; i >= 98; i--)
+		{
+			if (i != n)
+			{
+				putchar(',');
+                putchar(' ');
+			}
+			putchar(i + '0');
+		}
 	}
+	else
+	{
+		for (i = n; i <= 98 ; i++)
+		{
+			if (i != n)
+			{
+				putchar(',');
+                putchar(' ');
+			}
+			putchar(i + '0');
+		}
+	}
+	putchar('\n');
 }
