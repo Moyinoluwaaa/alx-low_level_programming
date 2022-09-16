eee5f1addeab5f033a3ff9db04914a5b6119511c
Moyinoluwaaa <omopariolafaith@gmail.com>
2022-09-16 03:00:53 -0700

    straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_line - func name
+ * @n: param name
+ *
+ * Return: ends
+ */
+
+void print_line(int n)
+{ 
+	int w = 0;
+
+	while(w < n)
+	{
+		w++;
+		putchar('_');
+	}
+	if (n <= 0);
+	{
+		putchar('\n');
+	} 
+return;
+}
