6918cca08450810d501611c0e7d819838dcbbdd0
Moyinoluwaaa <omopariolafaith@gmail.com>
2022-09-16 03:31:04 -0700

    diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line - func name
+ * @n: parameter name
+ *
+ * Return: void
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		putchar('\');
+	}
+	putchar('\n');
+}
