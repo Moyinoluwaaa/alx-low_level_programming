bf599b2216c3009f477fe0ba3e3fc3068b08dcec
Moyinoluwaaa <omopariolafaith@gmail.com>
2022-09-17 03:27:04 -0700

    fibu

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * main - func name
+ *
+ * Return: ends
+ */
+
+int main()
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+	if ((i % 3 == 0) && (i % 5 == 0))
+	{
+		printf("FizzBuzz");
+		printf(" ");
+	}
+	else if (i % 3 == 0)
+	{
+		printf("Fizz");
+		printf(" ");
+	}
+	else if (i % 5 == 0)
+	{
+		printf("Buzz");
+		printf(" ");
+	}
+	else
+	{
+		printf("%d", i);
+		printf(" ");
+	}
+	}
+	return (0);
+}
