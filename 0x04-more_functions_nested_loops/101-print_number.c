c6256adc1f8b40b1ae695b78615a36b0faa02003
Moyinoluwaaa <omopariolafaith@gmail.com>
2022-09-17 11:04:43 -0700

    dhf

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int putchar(char c);
+
+/**
+ * print_number - like a hello world
+ *
+ * @n: params an integer
+ *
+ * No return
+ */
+
+
+void print_number(int n)
+{
+
+	unsigned int n1 = 0;
+
+	if  (n < 0)
+	{
+		n1 = -n;
+		putchar('-');
+	}
+
+	else
+	{
+		n1 = n;
+	}
+
+	if (n1 / 10)
+	{
+		print_number(n1 / 10);
+	}
+
+	putchar((n1 % 10) + '0');
+}
