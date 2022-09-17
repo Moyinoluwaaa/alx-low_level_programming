fc2203e6f483a97b92063dbd9d36ce7d0e106999
Moyinoluwaaa <omopariolafaith@gmail.com>
2022-09-17 11:42:04 -0700

    dhf

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,16 +6,4 @@
  * @n: number.
  * Return: no return
  */
-void print_number(int n)
-{
-	if (n < 0)
-	{
-		putchar('-');
-		n = -n;
-	}
-	if (n / 10 != 0)
-	{
-		print_number(n / 10);
-	}
-	putchar(n % 10 + '0');
-}
+
