6a8d28e8aded9a6f2f3b882c983cc70b57879ae7
Moyinoluwaaa <omopariolafaith@gmail.com>
2022-09-17 11:50:58 -0700

    fh

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -4,18 +4,4 @@
  * print_number - prints an interger.
  *
  * @n: number.
- * Return: no return
- */
-void print_number(int n)
-{
-	if (n < 0)
-	{
-		putchar('-');
-		n = -n;
-	}
-	if (n / 10 != 0)
-	{
-		print_number(n / 10);
-	}
-	_putchar(n % 10 + '0');
-}
+ * Return: no retur
