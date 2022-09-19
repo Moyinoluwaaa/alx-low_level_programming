053a8333514e75fc65a9eedbe1996ea831df0c0a
Moyinoluwaaa <omopariolafaith@gmail.com>
2022-09-19 23:10:02 +0100

    as

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -15,3 +15,12 @@ putchar(str[i]);
 }
 putchar('\n');
 }
+
+int main(void)
+{
+    char *str;
+
+    str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+    _puts(str);
+    return (0);
+}
