bd28c7235b4ba40c4db7d84924a98d560cb8f31a
Moyinoluwaaa <omopariolafaith@gmail.com>
2022-09-20 00:14:45 +0100

    jk

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * puts2 - func name
+ * @str: djd
+ * Return: xdf
+ */
+
+void puts2(char *str)
+{
+    int i;
+    
+    for (i = 0; str[i] != '\0'; i+=2)
+    {
+        putchar(str[i]);
+    }
+}
