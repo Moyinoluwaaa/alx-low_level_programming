84267516d66dddd37685c034480a2972dd3077d1
Moyinoluwaaa <omopariolafaith@gmail.com>
2022-09-19 18:38:43 +0100

    point1

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/** 
+ * reset_to_98 - the func name
+ * @*n: param name
+ * 
+ * Return: returns void
+ */
+void reset_to_98(int *n)
+{
+    *n = 98;
+    return (0);
+}
