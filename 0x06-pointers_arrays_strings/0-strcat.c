648b692c79fc0f18f963188edc30687a7604c202
Moyinoluwaaa <omopariolafaith@gmail.com>
2022-09-21 13:11:24 +0100

    m

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,7 +11,8 @@ char *_strcat(char *dest, char *src)
 {
 int i, j, k;
 
-for(i = 0; dest[i] != '\0'; i++);
+for (i = 0; dest[i] != '\0'; i++);
+
 for (j = 0; src[j] != '\0'; j++, i++)
 {
 dest[i] = src[j];
