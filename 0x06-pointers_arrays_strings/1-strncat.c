ffdde5f94e34370e3abd1a5332bfbf5ccb42e997
Moyinoluwaaa <omopariolafaith@gmail.com>
2022-09-21 12:41:42 -0700

    djf

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,7 +9,7 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-int i,j;
+int i, j;
 
 j = 0;
 for (i = 0; i < 1000; i++)
@@ -25,5 +25,5 @@ for (i = 0; src[i] != '\0' && i < n; i++)
 dest[i + j] =  src[i];
 }
 dest[i + j] = '\0';
-return(dest);
+return (dest);
 }
