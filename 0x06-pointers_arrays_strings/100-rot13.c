93979076d30316e13f2e5a6aa45ae76c5cee9268
Moyinoluwaaa <omopariolafaith@gmail.com>
2022-09-22 16:43:13 -0700

    djf

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -8,23 +8,22 @@
 
 char *rot13(char *n)
 {
-	int i;
-	int j;
+int i;
+int j;
 
-	char data1[] = "ABCDEFGHIJKLMNOPQRnTUVWXYZabcdefghijklmnopqrntuvwxyz";
-	char datarot[] = "NOPQRnTUVWXYZABCDEFGHIJKLMnopqrntuvwxyzabcdefghijklm";
+char data1[] = "ABCDEFGHIJKLMNOPQRnTUVWXYZabcdefghijklmnopqrntuvwxyz";
+char datarot[] = "NOPQRnTUVWXYZABCDEFGHIJKLMnopqrntuvwxyzabcdefghijklm";
 
-	for (i = 0; n[i] != '\0'; i++)
-	{
-		for (j = 0; j < 52; j++)
-		{
-			if (n[i] == data1[j])
-			{
-				n[i] = datarot[j];
-				break;
-			}
-		}
-	}
-	return (n);
-
+for (i = 0; n[i] != '\0'; i++)
+{
+for (j = 0; j < 52; j++)
+{
+if (n[i] == data1[j])
+{
+n[i] = datarot[j];
+break;
+}
+}
+}
+return (n);
 }
