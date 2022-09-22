0591f8779c784efa9d45b86bbfc1851b4defae46
Moyinoluwaaa <omopariolafaith@gmail.com>
2022-09-22 23:38:18 +0100

    j

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -21,14 +21,3 @@ if (n[i] == delimeters[j] && n[i + 1] >= 'a' && n[i + 1] <= 'z')
 n[i + 1] = n[i + 1] - 32;
 }
 }
-
-int main(void)
-{
-    char str[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
-    char *ptr;
-
-    ptr = cap_string(str);
-    printf("%s", ptr);
-    printf("%s", str);
-    return (0);
-}
