c5b61ec0057b19c3835ff7faedcaf79f37fe802e
Moyinoluwaaa <omopariolafaith@gmail.com>
2022-09-23 00:34:28 +0100

    b

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rot13r - func name
+ * @n: param
+ * Return: xdf
+ */
+
+char *rot13(char *n)
+{
+    int i;
+	int j;
+	char data1[] = "ABCDEFGHIJKLMNOPQRnTUVWXYZabcdefghijklmnopqrntuvwxyz";
+	char datarot[] = "NOPQRnTUVWXYZABCDEFGHIJKLMnopqrntuvwxyzabcdefghijklm";
+
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (n[i] == data1[j])
+			{
+				n[i] = datarot[j];
+				break;
+			}
+		}
+	}
+	return (n);
+
+}
