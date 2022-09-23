1629b8acd2c5522b3d97548ae3f5de62e42790b4
Moyinoluwaaa <omopariolafaith@gmail.com>
2022-09-23 13:55:50 +0100

    ad

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,13 +1,36 @@
 #include "main.h"
 
 /**
- * print_number - func name
- * @n: parameter name
- * Return: ends
+ * print_number - prints an integer.
+ * @n: input integer.
+ * Return: no return.
  */
-
-
 void print_number(int n)
 {
+unsigned int m, d, count;
 
+if (n < 0)
+{
+_putchar(45);
+m = n * -1;
 }
+else
+{
+m = n;
+}
+
+d = m;
+count = 1;
+
+while (d > 9)
+{
+d /= 10;
+count *= 10;
+}
+
+for (; count >= 1; count /= 10)
+{
+_putchar(((m / count) % 10) + 48);
+}
+}
+
