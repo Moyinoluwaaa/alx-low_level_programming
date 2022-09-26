366a9465111c2c34f313931681f95b7bc89c3485
Moyinoluwaaa <omopariolafaith@gmail.com>
2022-09-26 11:06:45 +0100

    hj

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,4 +1,4 @@
-#include "main.h"
+q#include "main.h"
 
 /**
  * print_number - prints an integer.
---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memset - the func name
+ * @s: para1
+ * @b: para2
+ * @n: para3
+ * Return: returns the value
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+int i;
+
+for (i = 0; i < n; i++)
+{
+s[i] = b;
+}
+return(s);
+}
