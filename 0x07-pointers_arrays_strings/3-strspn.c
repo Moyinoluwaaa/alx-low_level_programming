db40ce4937be3e4f8e214eb16459bd0e1fa36ec1
Moyinoluwaaa <omopariolafaith@gmail.com>
2022-09-26 23:04:42 +0100

    m

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,5 +9,28 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-    
+int i, j, k;
+
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; accept[j] != s[i]; j++)
+{
+if (accept[j] == '\0')
+{
+return(i);
+}
+}
+} 
+return(i);
+}
+
+int main(void)
+{
+    char *s = "hello, world";
+    char *f = "oleh";
+    unsigned int n;
+
+    n = _strspn(s, f);
+    printf("%u\n", n);
+    return (0);
 }
