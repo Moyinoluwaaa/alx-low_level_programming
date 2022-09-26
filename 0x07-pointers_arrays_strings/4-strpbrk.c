4023aec354796136b24a74422ff1f5d9e3b61e3c
Moyinoluwaaa <omopariolafaith@gmail.com>
2022-09-26 23:27:43 +0100

    m

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -7,19 +7,19 @@
  * Return: returns the value
  */
 
-unsigned int _strspn(char *s, char *accept)
+char *_strpbrk(char *s, char *accept)
 {
 int i, j;
 
 for (i = 0; s[i] != '\0'; i++)
 {
-for (j = 0; accept[j] != s[i]; j++)
+for (j = 0; accept[j] != '\0'; j++)
 {
-if (accept[j] == '\0')
+if (accept[j] == s[i])
 {
-return (i);
+return (s + i);
 }
 }
 } 
-return (i);
+return (0);
 }
