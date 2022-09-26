f93134cdb199f4587be861c8936ae808f3215866
Moyinoluwaaa <omopariolafaith@gmail.com>
2022-09-26 21:11:45 +0100

    k

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -18,17 +18,3 @@ return (s);
 }
 return (s);
 }
-
-int main(void)
-{
-    char *s = "hello";
-    char *f;
-
-    f = _strchr(s, 'l');
-
-    if (f != NULL)
-    {
-        printf("%s\n", f);
-    }
-    return (0);
-}
