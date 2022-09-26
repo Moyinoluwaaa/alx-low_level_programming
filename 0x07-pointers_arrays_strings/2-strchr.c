9a8ac71092e14e6719db17b11acc22d855868946
Moyinoluwaaa <omopariolafaith@gmail.com>
2022-09-26 12:14:09 +0100

    z

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strchr - the func name
+ * @s: para1
+ * @c: para2
+ * Return: returns the value
+ */
+
+char *_strchr(char *s, char c)
+{
+
+for (; *s != '\0'; s++)
+{
+if (*s == c)
+{
+return (s);
+}
+        
+}
+return (0);
+}
