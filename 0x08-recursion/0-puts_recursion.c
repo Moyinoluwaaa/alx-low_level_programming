456ae7b2183917cb822cb2c6d07dfc284f50399c
Moyinoluwaaa <omopariolafaith@gmail.com>
2022-09-28 16:02:08 +0100

    k

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,25 +1,18 @@
 #include "main.h"
 
 /**
- * _strspn - the func name
+ * _puts_recursion - the func name
  * @s: para1
- * @accept: para2
  * Return: returns the value
  */
 
 void _puts_recursion(char *s)
 {
-    if (s[0] == '\0')
-    {
-        putchar('\n');
-        return;
-    }
-    putchar(s[0]);
-    _puts_recursion(s + 1);
-}
-
-int main(void)
+if (s[0] == '\0')
 {
-    _puts_recursion("Puts with recursion");
-    return (0);
+putchar('\n');
+return;
+}
+putchar(s[0]);
+_puts_recursion(s + 1);
 }
