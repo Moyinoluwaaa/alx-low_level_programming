40a5c99da6cd1056a71f4ac2d99b9d7052cec915
Moyinoluwaaa <omopariolafaith@gmail.com>
2022-10-01 23:30:41 +0100

    j

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - prints the main function
+ * @argc: msj
+ * @argv: jkl
+ * Return: gives feedback
+ */
+int main(int argc , char *argv[] __attribute__((unused)))
+{
+printf("%d", argc - 1);
+printf("\n");
+return (0);
+}
