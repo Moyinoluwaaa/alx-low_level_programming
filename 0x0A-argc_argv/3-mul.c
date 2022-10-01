a0bb7d8ce6f2dfd93a52cfda2a3dc52e15981cbf
Moyinoluwaaa <omopariolafaith@gmail.com>
2022-10-01 23:59:11 +0100

    k

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -5,14 +5,14 @@
  * @argv: jkl
  * Return: gives feedback
  */
+
 int main(int argc, char *argv[])
 {
 if (argc != 3)
 {
-printf("Error");
-printf("\n");
+printf("Error\n");
 return (1);
 }
-printf("%d \n", atoi(argv[1]) * atoi(argv[2]));
+printf("%d\n", atoi(argv[1]) + atoi(argv[2]));
 return (0);
 }
