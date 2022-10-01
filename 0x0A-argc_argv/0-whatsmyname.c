3e9fca54a95ae7da6f0d9ee140e48beaefe1b0d9
Moyinoluwaaa <omopariolafaith@gmail.com>
2022-10-01 23:26:48 +0100

    m

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - prints the main function
+ * @argc: msj
+ * @argv: jkl
+ * Return: gives feedback
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+printf("%s", argv[0]);
+printf("\n");
+return (0);
+}
