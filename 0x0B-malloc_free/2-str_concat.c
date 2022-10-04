3472a116db1d9a76ae82d56bc62fa5a1bdeffe41
Moyinoluwaaa <omopariolafaith@gmail.com>
2022-10-04 16:53:22 +0100

    k

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * *str_concat - creates array
+ * @s1: sizeof it
+ * @s2: wjw
+ * Return: returns
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+char *u;
+unsigned int i,j;
+
+u = (char *)malloc(sizeof(char) * i + j + 1);
+
+for (i = 0; s1[i] != '\0'; i++)
+{
+u[i] = s1[i];
+}
+for (j = 0; s2[j] != '\0'; i++, j++)
+{
+u[i] = s2[j];
+}
+if (u == NULL)
+return (NULL);
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
+return (u);
+}
