a246937820804634363095dea5264628f9020af3
Moyinoluwaaa <omopariolafaith@gmail.com>
2022-10-04 15:05:38 -0700

    hd

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -8,14 +8,14 @@
 
 int **alloc_grid(int width, int height)
 {
-unsigned int i,j;
+unsigned int i, j;
 int **a;
 
 if (width <= 0)
 return (NULL);
 if (height <= 0)
 return (NULL);
-a = malloc(sizeof(int) * (i+j));
+a = malloc(sizeof(int) * (i + j));
 if (a == NULL)
 return (NULL);
 for (i = 0; i < width; i++)
