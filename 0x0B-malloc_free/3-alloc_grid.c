948fb9744173077a4860fe058ad67dbbd7a3f321
Moyinoluwaaa <omopariolafaith@gmail.com>
2022-10-04 23:00:36 +0100

    kkn

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * **alloc_grid - creates array
+ * @width: sizeof it
+ * @height: wjw
+ * Return: returns
+ */
+
+int **alloc_grid(int width, int height)
+{
+unsigned int i,j;
+int **a;
+
+if (width <= 0)
+return (NULL);
+if (height <= 0)
+return (NULL);
+a = malloc(sizeof(int) * (i+j));
+if (a == NULL)
+return (NULL);
+for (i = 0; i < width; i++)
+{
+a[i] = malloc(width * sizeof(int));
+for (j = 0; j < height; j++)
+{
+a[j] = malloc(height * sizeof(int));
+a[i][j] = 0;
+}
+}
+return (a);
+}
