a8a90507c6206411fa339f91e70377163e0b362d
Moyinoluwaaa <omopariolafaith@gmail.com>
2022-10-04 16:11:36 +0100

    k

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -20,18 +20,3 @@ for (j = 0; j <= i; j++)
 strout[j] = str[j];
 return (strout);
 }
-
-int main(void)
-{
-    char *s;
-
-    s = _strdup("ALX SE");
-    if (s == NULL)
-    {
-        printf("failed to allocate memory\n");
-        return (1);
-    }
-    printf("%s\n", s);
-    free(s);
-    return (0);
-}
