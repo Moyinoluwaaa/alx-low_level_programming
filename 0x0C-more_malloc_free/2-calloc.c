00a9c8cec85c405e64a1899de9e084afe8571d0b
Moyinoluwaaa <omopariolafaith@gmail.com>
2022-10-06 16:06:24 +0100

    m

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -8,7 +8,8 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-char *meme = 0;
+char *meme;
+unsigned int i;
 
 meme = malloc(size * nmemb);
 if (nmemb == 0)
@@ -17,5 +18,45 @@ if (size == 0)
 return (NULL);
 if (meme == NULL)
 return (NULL);
+for (i = 0; i < (size * nmemb); i++)
+meme[i] = 0;
 return (meme);
+}
+void simple_print_buffer(char *buffer, unsigned int size)
+{
+    unsigned int i;
+
+    i = 0;
+    while (i < size)
+    {
+        if (i % 10)
+        {
+            printf(" ");
+        }
+        if (!(i % 10) && i)
+        {
+            printf("\n");
+        }
+        printf("0x%02x", buffer[i]);
+        i++;
+    }
+    printf("\n");
+}
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *a;
+
+    a = _calloc(98, sizeof(char));
+    strcpy(a, "Best");
+    strcpy(a + 4, " School! :)\n");
+    a[97] = '!';
+    simple_print_buffer(a, 98);
+    free(a);
+    return (0);
 }
