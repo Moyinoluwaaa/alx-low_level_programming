e3fb453a611c8a612e888d1a07606a642406056c
Moyinoluwaaa <omopariolafaith@gmail.com>
2022-10-06 15:55:43 +0100

    ,m

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,7 +10,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 char *meme = 0;
 
-meme = malloc(sizeof(size) * nmemb);
+meme = malloc(size * nmemb);
 if (nmemb == 0)
 return (NULL);
 if (size == 0)
