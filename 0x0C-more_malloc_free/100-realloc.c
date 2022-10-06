575c4a839bbd0cc7944bc6aa68c8761c4b5c0a5c
Moyinoluwaaa <omopariolafaith@gmail.com>
2022-10-06 18:16:52 +0100

    k

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * *_realloc - frees a 2 dimensional grid.
+ * @ptr: malloc
+ * @old_size: second
+ * @new_size: ajah
+ * Return: no return
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+unsigned int i;
+char *rt;
+
+ptr = malloc(old_size);
+if (new_size == old_size)
+return (ptr);
+if (ptr == NULL)
+{
+rt = malloc(new_size);
+if (rt == NULL)
+return (NULL);
+}
+if (new_size > old_size && (ptr != NULL))
+{
+rt = malloc(new_size);
+if (rt == NULL)
+return (rt);
+for (i = 0; i < old_size; i++)
+rt[i] = *((char *)ptr + 1);
+free(rt);
+}
+return (rt);
+}
