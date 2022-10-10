f67a8014d776db0cee9b09b01a2cc179b65ad467
Moyinoluwaaa <omopariolafaith@gmail.com>
2022-10-10 17:12:09 +0100

    jk

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * main - main func
- * Return: returns
+ * main - prints the name of the file program was compiled from
+ * Return: Always 0
  */
+
 int main(void)
 {
-putchar(__FILE__);
+printf("%s\n", __FILE__);
 return (0);
 }
