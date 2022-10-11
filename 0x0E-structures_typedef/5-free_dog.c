547f06dad62322697f21d7c035e848f084debdca
Moyinoluwaaa <omopariolafaith@gmail.com>
2022-10-11 15:39:28 +0100

    hi

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+/**
+ * free_dog - the declared
+ * @d: var
+ * Description: This struct is for dog
+ */
+void free_dog(dog_t *d)
+{
+if (d)
+{
+free(d->name);
+free(d->owner);
+free(d);
+}
+}
