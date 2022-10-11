f2c04000f093735eabfea73e53f792597920a1ce
Moyinoluwaaa <omopariolafaith@gmail.com>
2022-10-11 15:42:36 +0100

    f

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,29 +10,29 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	char *n, *o;
-	dog_t *new_dog = malloc(sizeof(dog_t));
+char *n, *o;
+dog_t *new_dog = malloc(sizeof(dog_t));
 
-	if (!new_dog || !name || !owner)
-	{
-		return (NULL);
-	}
-	n = malloc(_strlen(name) + 1);
-	if (!n)
-	{
-		return (free(new_dog), NULL);
-	}
-	n = _strdup(name);
-	new_dog->name = n;
-	o = malloc(_strlen(owner) + 1);
-	if (!o)
-	{
-		return (free(new_dog->name), free(new_dog), NULL);
-	}
-	o = _strdup(owner);
-	new_dog->owner = o;
-	new_dog->age = age;
-	return (new_dog);
+if (!new_dog || !name || !owner)
+{
+return (NULL);
+}
+n = malloc(_strlen(name) + 1);
+if (!n)
+{
+return (free(new_dog), NULL);
+}
+n = _strdup(name);
+new_dog->name = n;
+o = malloc(_strlen(owner) + 1);
+if (!o)
+{
+return (free(new_dog->name), free(new_dog), NULL);
+}
+o = _strdup(owner);
+new_dog->owner = o;
+new_dog->age = age;
+return (new_dog);
 }
 
 /**
@@ -43,13 +43,13 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 int _strlen(char *s)
 {
-	char *p = s;
+char *p = s;
 
-	while (*s)
-	{
-		s++;
-	}
-	return (s - p);
+while (*s)
+{
+s++;
+}
+return (s - p);
 }
 
 /**
@@ -61,23 +61,23 @@ int _strlen(char *s)
 
 char *_strdup(char *str)
 {
-	int i, len;
-	char *copy;
+int i, len;
+char *copy;
 
-	if (!str)
-	{
-		return (NULL);
-	}
-	len = _strlen(str);
-	copy = malloc(sizeof(char) * len + 1);
-	if (!copy)
-	{
-		return (NULL);
-	}
-	for (i = 0; i < len; i++)
-	{
-		copy[i] = str[i];
-	}
-	copy[i] = 0;
-	return (copy);
+if (!str)
+{
+return (NULL);
+}
+len = _strlen(str);
+copy = malloc(sizeof(char) * len + 1);
+if (!copy)
+{
+return (NULL);
+}
+for (i = 0; i < len; i++)
+{
+copy[i] = str[i];
+}
+copy[i] = 0;
+return (copy);
 }
