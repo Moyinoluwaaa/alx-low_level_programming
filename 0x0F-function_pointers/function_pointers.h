d5150d53938260df2663d2d051e3c23ba9e6b910
Moyinoluwaaa <omopariolafaith@gmail.com>
2022-10-12 20:00:41 +0100

    ,

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,6 @@
-#ifndef FUNCTION_H
-#define FUNCTION_H
-
+#ifndef _function_pointers_
+#define _function_pointers_
+#include <stdio.h>
 #include <stddef.h>
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
