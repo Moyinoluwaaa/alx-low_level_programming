e068dbe0158169c46c42b99646c9a7a413dd49e6
Moyinoluwaaa <omopariolafaith@gmail.com>
2022-10-12 19:49:58 +0100

    klo

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,2 +1,9 @@
-
+#include "function_pointers.h"
+/**
+ * print_name - prints a name
+ * @array: param
+ * @size: param
+ * @action: ndif
+ * Return: rer
+ */
 void array_iterator(int *array, size_t size, void (*action)(int));
