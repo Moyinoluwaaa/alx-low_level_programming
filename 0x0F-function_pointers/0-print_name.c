799b714a8a45bff8803ff6ff15787eb81bbd7e11
Moyinoluwaaa <omopariolafaith@gmail.com>
2022-10-12 19:53:45 +0100

    k

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,12 +1,15 @@
 #include "function_pointers.h"
+
 /**
  * print_name - prints a name
- * @name: param
- * @f: param
- * Return: rer
+ * @name: input params for name
+ * @f: reps a function pointer
  */
+
 void print_name(char *name, void (*f)(char *))
 {
-if (name && f)
-f(name);
+	if (name && f)
+	{
+		f(name);
+	}
 }
